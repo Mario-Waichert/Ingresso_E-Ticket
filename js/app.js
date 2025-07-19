b28005e53152509735781caf7b4d75357fd19f05
Mario Waichert <waichert.mario@gmail.com>
2025-07-19 18:42:31 +0100

    Inicio, construção da função comprar

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+    
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+
+ 
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert ('Quantidade solicitada indisponível para Pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert ('Compra realizada com sucesso!');
+    }
+}
